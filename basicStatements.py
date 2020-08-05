b7c9115fff1a4522b04387a794d26557623508bc
IshTheGreat <65795098+IshTheGreat@users.noreply.github.com>
2020-08-04 18:34:16 -0700

    Create basicStatements.py

---
+++ basicStatements.py
@@ -0,0 +1,23 @@
+#Hey everyone, welcome to my channel, and most importantly, welcome to python!
+#This video will teach you a couple of basic statemetents in python, check my other video to learn how to dowload a text editor to type and run python code! 
+#It will be in the description!
+
+print("Hello World") #This sentence is called the print statement, you say print, then parenthisis and enter something
+                    # in the quatiation marks!
+print("HI Everyone!!!") #Lets run this now! Look at the bottom of the screen, the thing hilighted in blue!
+
+#LETS talk about variables!
+#Syntax = variable name = variable value
+var = "python is amazing" #var is the variable name, and the STR python is amazing is the str!
+                          #a string or STR is like a sentence that is put in "". STR's are data types
+print(var)                #Ok guys can you tell me what would be shown in the terminal when I run this program??
+#Lets run this! If you guys guessed that it was gonna print my str that i defined the variable var from in the prior line!
+
+#Now, you guys might me guessing, why am I adding hashtags before every sentence that I type! Well these are called comments!
+#Comments are pieces of code that are not read when the system reads a file!
+
+#Whatever I type, the system wont read!
+
+THANK YOU for watching Python Intro, I will add a gitub link to these lines of code in the description,
+    See u later
+BYE!!!!
